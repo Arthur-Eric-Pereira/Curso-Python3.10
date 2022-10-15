b259fc3b3fd43b4d77c10484196330bc13e12a1a
Arthur Eric Pereira <114352981+Arthur-Eric-Pereira@users.noreply.github.com>
2022-10-15 15:13:19 -0300

    Atualizei o código!
    >  Uma correção simples

--- Aula01(introdução)/01-print( ).py
+++ Aula01(introdução)/01-print( ).py
@@ -5,7 +5,7 @@ print('___'*12)
 print('\_/'*12)
 print('=-='*12)
 
-print('''       title
+print('''       início:
 ........................
 ........................
 ........................
@@ -16,8 +16,8 @@ fim...''')
 número = 17.7895
 print(f'>> {número} = 2 casas depois da virgula: {número:.2f}')
 
-print()
-print()
+print()  #pula pra próxima linha
+print()  #pula pra próxima linha
 A = 'ARTHUR PEREIRA'
 print(f'{A:|^44}')
 print()
